04c103949da46bc3a6d75d830311448742ee2d93
Shahid <shahidansari2452@gmail.com>
2024-06-16 23:33:51 +0530

    created necessary folders and started to work on home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="text-sky-400">
-      Hello World
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      {/* Section 1 */}
+      
+
+      {/* Section 2 */}
+
+      {/* Section 3 */}
+
+      {/* Section 4 */}
+    </div>
+  )
+}
+
+export default Home
